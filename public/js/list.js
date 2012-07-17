ab76054c079c20ef8df1db8115c8cb54bfc36e64
Paul Zaich <pzaich@gmail.com>
2012-07-16 20:38:22 -0700

    marcus' edits

--- public/js/list.js
+++ public/js/list.js
@@ -1,24 +1,35 @@
-var TaskList = function(){
-  this.entries = [];
-  this.task_counter = 0;
-  this.return_entries = function () {
-    return this.entries;
-  }
-  this.addTask = function(description, date) {
+var taskList = {
+  
+  task_counter: 0,
+
+  entries: $.map($.totalStorage("savedlist") || [], function(entry, i){
+    return new Task(entry.task_description, entry.date, i, entry.completion);
+  }),
+  
+  addTask: function(description, date) {
     var newTask = new Task(description, date, this.task_counter, false);
     this.entries.push(newTask);
     this.task_counter++;
     listSaver.save();
-  }
+  },
   
-  this.markComplete = function(index) {
+  markComplete: function(index) {
     this.entries[index].complete();
     listSaver.save();
-  }
+  },
   
-  this.markIncomplete = function(index) {
+  markIncomplete: function(index) {
     this.entries[index].setIncomplete();
     listSaver.save();
-  }
+  },
+
+  save: function () {
+    // console.log("just saved an item");
+    $.totalStorage("savedlist", this);
+  },
   
-}
+  not_empty: function() {
+    return $.totalStorage("savedlist");
+  }
+
+};
